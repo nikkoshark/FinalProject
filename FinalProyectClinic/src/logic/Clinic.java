8955ed4756b6affe93a58d9aa8499ead3705f0ff
HP <HP@DESKTOP-VGB2B7A>
2023-07-10 20:02:58 -0400

    VER 0: ready?

---
+++ FinalProyectClinic/src/logic/Clinic.java
@@ -0,0 +1,9 @@
+package logic;
+
+public class Clinic {
+
+	public Clinic() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- FinalProyectClinic/src/logic/Hola.java
+++ FinalProyectClinic/src/logic/Hola.java
@@ -3,7 +3,7 @@ package logic;
 public class Hola {
 
 	public Hola() {
-		// TODO Auto-generated constructor stub
+		// aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 	}
 
 }
